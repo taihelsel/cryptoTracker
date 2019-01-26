952fdcda0e0de82a674b394fb4c01e65e92eff4f
taihelsel <tai.bayard@gmail.com>
2019-01-26 02:04:19 -0800

    added auth components

--- client/src/App.js
+++ client/src/App.js
@@ -4,25 +4,16 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 /*Components*/
 import Home from "./components/Home/Home.js";
+import Login from "./components/Auth/Login.js";
+import SignUp from "./components/Auth/SignUp.js";
 class App extends Component {
-  componentDidMount() {
-    fetch("/test")
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          console.warn("error making test request");
-        }
-      })
-      .then(data => {
-        console.log(data);
-      });
-  }
   render() {
     return (
       <Router>
         <div className="App">
           <Route exact path="/" component={Home} />
+          <Route exact path="/auth/login" component={Login} />
+          <Route exact path="/auth/signup" component={SignUp} />
         </div>
       </Router>
     );
