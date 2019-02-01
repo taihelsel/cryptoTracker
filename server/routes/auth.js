5bf4f1750175efb4a21c0b28510b5393d9b8c745
taihelsel <tai.bayard@gmail.com>
2019-02-01 10:33:00 -0800

    formatting

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,25 +1,24 @@
 const express = require("express"),
-router = express.Router(),
-mongoose = require("mongoose"),
-User = require(".././models/User"),
-jwt = require("jsonwebtoken");
+    router = express.Router(),
+    mongoose = require("mongoose"),
+    User = require(".././models/User"),
+    jwt = require("jsonwebtoken");
 
 //POST
 router.post("/login", (req, res) => {
-    
+
 });
 router.post("/signup", (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     const passwordConf = req.body.passwordConf;
-    console.log("username",username,"password",password,"passwordConf",passwordConf);
-    if(password!==passwordConf) res.status(400).send({message:"Error passwords do not match"});
+    if (password !== passwordConf) res.status(400).send({ message: "Error passwords do not match" });
     else {
-        let newUser = new User({username});
+        let newUser = new User({ username });
         newUser.password = newUser.encryptPassword(password);
-        newUser.save(function(err,user){
-            if(err) res.status(400).send({ message:err});
-            else{
+        newUser.save(function (err, user) {
+            if (err) res.status(400).send({ message: err });
+            else {
                 delete user.password;
                 console.log(user);
                 res.json(user);
